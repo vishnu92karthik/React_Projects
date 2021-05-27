580d447587556b43c733026bd309c5be91f5b769
vishnu92karthik <vishnumkarthik@gmail.com>
2021-05-27 19:56:07 +0530

    CSS

--- E-commerce/src/components/Header.js
+++ E-commerce/src/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function Header(props){
     return (
-     <header>
+     <header class= "row block center">
          <div>
              <a href= "#/">
                  <h1>Small Shopping Cart</h1>
              </a>
          </div>
          <div>
-             <a href="#/cart">Cart</a>
-             <a href="#/signin">SignIn</a>
+             <a href="#/cart">Cart</a>  <a href="#/signin">SignIn</a>
+            
          </div>
      </header>   
     );
