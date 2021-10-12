34237f143361b8db9764d59f3f3623d5b357dd14
vishnu <vishnumkarthik@gmail.com>
2021-10-12 22:19:11 +0530

    App

--- BookSearch/src/App.js
+++ BookSearch/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import BooksSearch from './BookSearch';
+import Books from './Books';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-     <BooksSearch />
+    <Books />
     </div>
   );
 }
