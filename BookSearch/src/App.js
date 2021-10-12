c7bee088bf299d54d4227a92aef8daa191a0c87b
vishnu <vishnumkarthik@gmail.com>
2021-10-12 18:14:15 +0530

    BookSearch

---
+++ BookSearch/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BooksSearch from './BookSearch';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+     <BooksSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ BookSearch/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <div className= "header">
+                <h1>Book Search</h1>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
