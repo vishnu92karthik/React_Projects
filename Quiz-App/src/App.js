edb651f720c31d6ca6d4c45dc26261744d0f66e8
vishnu <vishnumkarthik@gmail.com>
2021-10-14 21:01:08 +0530

    Quiz-App

--- Quiz-App/src/App.js
+++ Quiz-App/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+
+import { useState,useEffect } from "react";
 import './App.css';
+import Quiz from "./Quiz";
 
 function App() {
+  const [quiz , setquiz] = useState();
+  const [question , setQuestions] = useState();
+  useEffect(() => {
+    async function getQuiz(){
+      const url = "https://opentdb.com/api.php?amount=50&category=9&type=multiple";
+      const response = await fetch(url);
+      const data = await response.json()
+      setquiz(data)
+      
+    }
+    return () => {
+      getQuiz()
+    }
+  }, [])
   return (
     <div className="App">
-    
+      {data && 
+      data.map(qest =>{
+        {quest.question}
+        {quest.correct_answer}
+      })}
+    <Quiz  />
     </div>
   );
 }
